e5890f6972946f424642931acfac497a3c903beb
adriano-devPOO <desenvolvedor.adriano@gmail.com>
2021-03-09 21:58:16 -0300

    Método create implementado na categoria

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Categoria } from './categoria.model';
@@ -9,9 +10,21 @@ import { Categoria } from './categoria.model';
 })
 export class CategoriaService {
 
-  constructor(private http: HttpClient) { } 
+  constructor(private http: HttpClient, private _snack: MatSnackBar) { } 
 
   findAll():Observable<Categoria[]>{
-    return this.http.get<Categoria[]>( 'https://recup-pos-unit.herokuapp.com/categorias')
+    return this.http.get<Categoria[]>('https://recup-pos-unit.herokuapp.com/categorias')
+  }
+
+  create(categoria: Categoria): Observable<Categoria>{
+    return this.http.post<Categoria>('https://recup-pos-unit.herokuapp.com/categorias', categoria);
+  }
+
+  mensagem(str: String): void{
+    this._snack.open(`${str}`, 'OK', {
+      horizontalPosition: 'end',
+      verticalPosition: 'top',
+      duration: 3000
+    })
   }
 }
