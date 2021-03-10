57c5ef6ee60be91355eb769751ee354abb93ffc8
adriano-devPOO <desenvolvedor.adriano@gmail.com>
2021-03-09 23:52:05 -0300

    Categoria delete implementada

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { CategoriaDeleteComponent } from './categoria-delete/categoria-delete.component';
 import { Categoria } from './categoria.model';
 
 @Injectable({
@@ -10,14 +11,26 @@ import { Categoria } from './categoria.model';
 })
 export class CategoriaService {
 
+  
+
   constructor(private http: HttpClient, private _snack: MatSnackBar) { } 
 
   findAll():Observable<Categoria[]>{
+    
     return this.http.get<Categoria[]>('https://recup-pos-unit.herokuapp.com/categorias')
   }
 
+  findById(id: String): Observable<Categoria>{
+    return this.http.get<Categoria>('https://recup-pos-unit.herokuapp.com/categorias/' + id)
+  }
+
   create(categoria: Categoria): Observable<Categoria>{
+   
     return this.http.post<Categoria>('https://recup-pos-unit.herokuapp.com/categorias', categoria);
+  }
+
+  delete(id: String): Observable<void>{
+    return this.http.delete<void>('https://recup-pos-unit.herokuapp.com/categorias/' + id)
   }
 
   mensagem(str: String): void{
